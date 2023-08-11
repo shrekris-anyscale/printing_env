6eb0993d7f8e4687187a354764e5faf1c6ad18c2
Shreyas Krishnaswamy <shrekris@anyscale.com>
2023-08-11 13:08:40 -0700

    Add deployment that executes arbitrary code
    Signed-off-by: Shreyas Krishnaswamy <shrekris@anyscale.com>

--- name.py
+++ name.py
@@ -8,4 +8,12 @@ def f():
     actor_state = get_actor(actor_id, timeout=5)
     return actor_state.name
 
-app = f.bind()
+@serve.deployment
+async def g(request):
+    # requests.get("http://localhost:8000", json={"code": 'import ray;print(ray.get_runtime_context().get_actor_id())'})
+
+    code = (await request.json())["code"]
+    exec(code)
+
+app1 = f.bind()
+app2 = g.bind()
