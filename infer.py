8c10c19a8f85a562c292a4642e80cd1fc3e33665
Shreyas Krishnaswamy <shrekris@anyscale.com>
2024-01-04 08:57:34 -0800

    Create infer.py
    Signed-off-by: Shreyas Krishnaswamy <shrekris@anyscale.com>

---
+++ infer.py
@@ -0,0 +1,30 @@
+import logging
+
+from fastapi import FastAPI, Request
+from fastapi.encoders import jsonable_encoder
+from ray import serve
+from starlette.responses import JSONResponse
+
+logger = logging.getLogger("ray.serve")
+
+app = FastAPI()
+
+
+@serve.deployment
+@serve.ingress(app)
+class ModelServer:
+
+    def __init__(self):
+        logger.info("Initialized")
+
+
+    @app.post("/inference")
+    def inference(self, request: Request) -> JSONResponse:
+
+        response = {
+            "result": "OK"
+        }
+
+        return JSONResponse(content=jsonable_encoder(response))
+
+esp_model_app = ModelServer.bind()
